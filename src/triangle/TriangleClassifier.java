a6df7e6ca3e7098a1e06a81e26dcf46461d7728b
hungtb131 <66382816+hungtb131@users.noreply.github.com>
2020-07-15 23:50:09 +0700

    add comment triangle

--- src/triangle/TriangleClassifier.java
+++ src/triangle/TriangleClassifier.java
@@ -2,6 +2,8 @@ package triangle;
 
 public class TriangleClassifier {
     public static String classifyTriangle(double firstSide, double secondSide, double thirdSide) {
+        //xét theo 3 góc có được không ?
+        //Tổng 3 góc là 180 2 góc bằng nhau,3 góc bằng nhau,1 góc vuông??
         if (firstSide + secondSide > thirdSide && secondSide + thirdSide > firstSide && thirdSide + firstSide > secondSide) {
             if (firstSide == secondSide && firstSide != thirdSide) return "isosceles triangle";
             if (thirdSide == secondSide && firstSide != thirdSide) return "isosceles triangle";
